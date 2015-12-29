18a47717756e40083a20dd4df5228a10a8e75d8f
Dixanta Shrestha <dixanta@gmail.com>
2015-12-29 11:12:22 +0545

    First Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var path=require("path");
+
+
+var app=express();
+
+var port=9090;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"bower_components")));
+
+app.get("/",function(req,res){
+	res.render('index',{
+		title:'Node App',
+		message:'Welcome to Node JS Application'
+	});
+});
+
+app.listen(port,function(){
+	console.log("Server is listening at "+ port);
+});
